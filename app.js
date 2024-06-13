4dc9783033300b3060ccf291d6fba7be4e4f5ac4
bipinlongjam <bipinlongjam11@gmail.com>
2024-06-13 11:20:29 +0530

    add input tag

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const http = require('http')
 
 const express = require('express');
-const { error } = require('console');
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = 3000;
 const server = http.createServer(app)
 
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req, res, next) =>{
-    console.log("In the middleware")
+app.use('/',(req, res, next) =>{
+    console.log("This always runs!")
     next();
 })
-app.use((req, res, next) =>{
-    console.log('In another middleware')
+app.use('/add-product',(req, res, next) =>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="number" name="size"><button type="submit">Add Product</button></form>')
+})
+
+app.use('/product', (req, res, next) =>{
+   console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/',(req, res, next) =>{
     res.send('<h1>Hello from Express</h1>')
 })
 server.listen(PORT,() =>{
