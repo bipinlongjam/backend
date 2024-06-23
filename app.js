6ac496c1ceb1fbec41b15432faca0efc46a3240d
bipinlongjam <bipinlongjam11@gmail.com>
2024-06-24 01:06:02 +0530

    add routes

--- app.js
+++ app.js
@@ -7,24 +7,18 @@ const app = express();
 const PORT = 3000;
 const server = http.createServer(app)
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/',(req, res, next) =>{
-    console.log("This always runs!")
-    next();
-})
-app.use('/add-product',(req, res, next) =>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="number" name="size"><button type="submit">Add Product</button></form>')
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) =>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.use('/product', (req, res, next) =>{
-   console.log(req.body);
-    res.redirect('/')
-})
-
-app.use('/',(req, res, next) =>{
-    res.send('<h1>Hello from Express</h1>')
-})
 server.listen(PORT,() =>{
         console.log("server started")
 })
